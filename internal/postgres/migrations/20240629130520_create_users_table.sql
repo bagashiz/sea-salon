0f3ea3cd1c5e5704027d485bcbfe30282396b64b
Bagas Hizbullah <work.bitsy@simplelogin.com>
2024-07-01 10:49:11 +0000

    feat(db): add admin user

--- internal/postgres/migrations/20240629130520_create_users_table.sql
+++ internal/postgres/migrations/20240629130520_create_users_table.sql
@@ -10,6 +10,19 @@ CREATE TABLE IF NOT EXISTS users (
     created_at TIMESTAMP DEFAULT now(),
     updated_at TIMESTAMP DEFAULT now()   
 );
+INSERT INTO users (
+    role,
+    email,
+    password,
+    full_name,
+    phone
+) VALUES (
+    'admin',
+    'thomas.n@compfest.id',
+    '$2a$10$4tS9MQtS6l/9PWY.MiR8O.3.yFKHvaB34kpQVGgSVnGrla6ztOaam',
+    'Thomas N',
+    '08123456789'
+);
 
 -- +goose Down
 DROP TABLE IF EXISTS users;
