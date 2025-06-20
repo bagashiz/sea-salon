d1dc959f82090b67531e1de0935a6690cfafc231
Bagas Hizbullah <work.bitsy@simplelogin.com>
2025-06-21 00:06:14 +0700

    feat: minimal tanstack counter app

---
+++ src/routes/index.tsx
@@ -0,0 +1,48 @@
+import { file, write } from "bun";
+import { createFileRoute, useRouter } from "@tanstack/react-router";
+import { createServerFn } from "@tanstack/react-start";
+
+const filePath = "count.txt";
+
+async function readCount() {
+  const counter = await file(filePath)
+    .text()
+    .catch(() => "0");
+  return parseInt(counter);
+}
+
+const getCount = createServerFn({
+  method: "GET",
+}).handler(() => {
+  return readCount();
+});
+
+const updateCount = createServerFn({ method: "POST" })
+  .validator((d: number) => d)
+  .handler(async ({ data }) => {
+    const count = await readCount();
+    await write(filePath, `${count + data}`);
+  });
+
+export const Route = createFileRoute("/")({
+  component: Home,
+  loader: async () => await getCount(),
+});
+
+function Home() {
+  const router = useRouter();
+  const state = Route.useLoaderData();
+
+  return (
+    <button
+      type="button"
+      onClick={() => {
+        updateCount({ data: 1 }).then(() => {
+          router.invalidate();
+        });
+      }}
+    >
+      Add 1 to {state}?
+    </button>
+  );
+}
