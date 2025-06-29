ef0d611e5a1f62e203506fc8d945e92c5a72e356
Bagas Hizbullah <work.bitsy@simplelogin.com>
2025-06-30 00:13:38 +0700

    fix: better seo

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -20,6 +20,18 @@ export const Route = createRootRoute({
       {
         title: "TanStack Start Starter",
       },
+      {
+        name: "description",
+        content: "TanStack Start Starter",
+      },
+    ],
+    scripts: [
+      process.env.NODE_ENV === "development"
+        ? {
+            crossOrigin: "anonymous",
+            src: "//unpkg.com/react-scan/dist/auto.global.js",
+          }
+        : undefined,
     ],
   }),
   component: RootComponent,
@@ -36,14 +48,8 @@ function RootComponent() {
 
 function RootDocument({ children }: Readonly<{ children: ReactNode }>) {
   return (
-    <html>
+    <html lang="en">
       <head>
-        {process.env.NODE_ENV === "development" && (
-          <script
-            crossOrigin="anonymous"
-            src="//unpkg.com/react-scan/dist/auto.global.js"
-          />
-        )}
         <HeadContent />
       </head>
       <body>
